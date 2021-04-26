81315b30e242d52d4e61cf5607d01703066bc491
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2021-04-25 19:11:22 -0700

    create the connecttion between database and app.

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ app.use(express.urlencoded({extended:true})); // to decode parameters that was s
 app.get("/", async function(req, res){
   res.render("partials/header");
 });
+
+
+//functions
+async function executeSQL(sql, params){
+  return new Promise (function (resolve, reject) {
+    pool.query(sql, params, function (err, rows, fields) {
+      if (err) throw err;
+        resolve(rows);
+    });
+  });
+}//executeSQL
+
+//values in red must be updated
+function dbConnection(){
+   const pool  = mysql.createPool({
+      connectionLimit: 10,
+      host: "",
+      user: "",
+      password: "",
+      database: ""
+   }); 
+   return pool;
+} //dbConnection
+
+
+//start server
+app.listen(3000, () => {
+console.log("Expresss server running...")
+} )
